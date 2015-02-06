cd5c831095565b9acfe612f14e4a5cc80edf45ef
Qinfeng Chen <qchen@palantir.com>
2015-02-06 11:02:31 -0500

    hides edge label when edge is hidden

--- plugins/sigma.renderers.edgeLabels/sigma.svg.edges.labels.def.js
+++ plugins/sigma.renderers.edgeLabels/sigma.svg.edges.labels.def.js
@@ -58,7 +58,7 @@
           size = edge[prefix + 'size'] || 1;
       // Case when we don't want to display the label
       if (!settings('forceLabels') && size < settings('edgeLabelThreshold') ||
-          source === target) {
+          source === target || edge.hidden) {
         text.style.display = 'none';
         return;
       }
