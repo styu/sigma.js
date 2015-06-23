42a8b370105bb45bb4bf24c084a2bef1b6a875aa
David Lipowicz <dlipowicz@palantir.com>
2015-06-23 14:33:32 -0700

    Made a few small changes to address merge comments given.

--- src/renderers/svg/sigma.svg.edges.directed.js
+++ src/renderers/svg/sigma.svg.edges.directed.js
@@ -18,13 +18,13 @@
      * @param  {configurable}             settings   The settings function.
      */
     create: function(edge, source, target, settings) {
-      var color = edge.color,
-          prefix = settings('prefix') || '',
-          edgeColor = settings('edgeColor'),
-          defaultNodeColor = settings('defaultNodeColor'),
+      var edgeColor = settings('edgeColor'),
+          color = edge.color,
           defaultEdgeColor = settings('defaultEdgeColor'),
+          defaultNodeColor = settings('defaultNodeColor'),
+          lineName = "line",
           polyName = "polygon",
-          lineName = "line";
+          prefix = settings('prefix') || '';
 
       if (!color)
         switch (edgeColor) {
@@ -39,24 +39,24 @@
             break;
         }
     
-        var parent = document.createElementNS(settings("xmlns"), "g"),
-            line = document.createElementNS(settings("xmlns"), lineName),
-            poly = document.createElementNS(settings("xmlns"), polyName);
+      var parent = document.createElementNS(settings("xmlns"), "g"),
+          line = document.createElementNS(settings("xmlns"), lineName),
+          poly = document.createElementNS(settings("xmlns"), polyName);
 
-        parent.appendChild(line);
-        parent.appendChild(poly);
+      parent.appendChild(line);
+      parent.appendChild(poly);
 
-        parent.setAttributeNS(null, "data-edge-id", edge.id);
+      parent.setAttributeNS(null, "data-edge-id", edge.id);
 
-        parent.setAttributeNS(null, "class", settings("classPrefix") + "-edge");
-        line.setAttributeNS(null, "class", settings("classPrefix") + "-edge-line");
-        poly.setAttributeNS(null, "class", settings("classPrefix") + "-edge-arrow");
+      parent.setAttributeNS(null, "class", settings("classPrefix") + "-edge");
+      line.setAttributeNS(null, "class", settings("classPrefix") + "-edge-line");
+      poly.setAttributeNS(null, "class", settings("classPrefix") + "-edge-arrow");
 
-        line.setAttributeNS(null, 'stroke', color);
-        poly.setAttributeNS(null, 'stroke', color);
-        poly.setAttributeNS(null, 'fill', color);
+      line.setAttributeNS(null, 'stroke', color);
+      poly.setAttributeNS(null, 'stroke', color);
+      poly.setAttributeNS(null, 'fill', color);
 
-        return parent;
+      return parent;
     },
 
     /**
@@ -69,76 +69,85 @@
      * @param  {configurable}             settings   The settings function.
      */
     update: function(edge, obj, source, target, settings) {
-        var prefix = settings("prefix") || "",
-            polyName = "polygon",
-            lineName = "line",
-            line = obj.getElementsByTagName(lineName)[0];
+	  // Don't try and draw an arrow from one point to the same.
+      if(source == target) return this;
 
-        // Draw a line from the source to the target.
-        line.setAttributeNS(null, "stroke-width", edge[prefix + "size"] || 1);
-        line.setAttributeNS(null, "x1", source[prefix + "x"]);
-        line.setAttributeNS(null, "y1", source[prefix + "y"]);
-        line.setAttributeNS(null, "x2", target[prefix + "x"]);
-        line.setAttributeNS(null, "y2", target[prefix + "y"]);
+      var prefix = settings("prefix") || "",
+          polyName = "polygon",
+          lineName = "line",
+          line = obj.getElementsByTagName(lineName)[0];
 
-        obj.style.display = "";
+      var sourceX = source[prefix + "x"];
+      var sourceY = source[prefix + "y"];
+      var targetX = target[prefix + "x"];
+      var targetY = target[prefix + "y"];
+      var targetSize = target[prefix + "size"];
 
-        // Don't try and draw an arrow from one point to the same.
-        if(source == target) return this;
 
-        var poly = obj.getElementsByTagName(polyName)[0];
+      // Draw a line from the source to the target.
+      line.setAttributeNS(null, "stroke-width", edge[prefix + "size"] || 1);
+      line.setAttributeNS(null, "x1", sourceX);
+      line.setAttributeNS(null, "y1", sourceY);
+      line.setAttributeNS(null, "x2", targetX);
+      line.setAttributeNS(null, "y2", targetY);
 
-        // Get the width and height of the line.
-        var lineWidth = source[prefix + "x"] - target[prefix + "x"];
-        var lineHeight = source[prefix + "y"] - target[prefix + "y"];
-        // Calculate the length of the line.
-        var lineLength = Math.sqrt(Math.pow(lineWidth, 2) + Math.pow(lineHeight, 2));
-        // Work out cos and sin of the triangle.
-        var cosAng = lineWidth / lineLength;
-        var sinAng = lineHeight / lineLength;
-        // Calculate the distance from the center of the node rectangle to the points
-        // at which the line exits the rectangle.
-        var distToEndWidth = target[prefix + "size"] / cosAng;
-        var distToEndHeight = target[prefix + "size"] / sinAng;
+      obj.style.display = "";
 
-        var arrowTriangleRatio = target[prefix + "size"] / 4;
-        var arrowTriangleWidth =  arrowTriangleRatio * 3;
-        var arrowTriangleHeight = arrowTriangleRatio * 5;
+      var poly = obj.getElementsByTagName(polyName)[0];
 
-        var endX = target[prefix + "x"];
-        var endY = target[prefix + "y"];
-        // Whichever distance is smaller is the point at which the line exits
-        // the rectangle. This is therefore where we want to place the arrow.
-        if (Math.abs(distToEndWidth) > Math.abs(distToEndHeight)) {
-            // The exact x,y coordinate pair of where the line exits through
-            // the top/bottom of the rectangle.
-            endX += ((distToEndHeight > 0) ? 1 : -1) * distToEndHeight * cosAng;
-            endY += ((distToEndHeight > 0) ? 1 : -1) * target[prefix + "size"];
-        } else {
-            // The exact x,y coordinate pair of where the line exits through
-            // the left/right of the rectangle.
-            endX += ((distToEndWidth > 0) ? 1 : -1) * target[prefix + "size"];
-            endY += ((distToEndWidth > 0) ? 1 : -1) * distToEndWidth * sinAng;
-        }
+      // Get the width and height of the line.
+      var lineWidth = sourceX - targetX;
+      var lineHeight = sourceY - targetY;
+      // Calculate the length of the line.
+      var lineLength = Math.sqrt(Math.pow(lineWidth, 2) + Math.pow(lineHeight, 2));
+      // Work out cos and sin of the triangle.
+      var cosAng = (lineLength == 0) ? 0 : (lineWidth / lineLength);
+      var sinAng = (lineLength == 0) ? 0 : (lineHeight / lineLength);
+      // Calculate the distance from the center of the node rectangle to the points
+      // at which the line exits the rectangle.
+      var distToEndWidth = (cosAng == 0) ? 0 : (targetSize / cosAng);
+      var distToEndHeight = (sinAng == 0) ? 0 : (targetSize / sinAng);
 
-        var angle = Math.asin(sinAng) * 180 / Math.PI;
-        // Account for which side the arrow is coming from.
-        if (distToEndWidth > 0) {
-            angle = 180 - angle;
-        }
-        // Draw a triangle on the screen. The arrow head is pointing to endX,endY.
-        // The other two points are placed to the left, at a
-        // height of + triHeight/2 and - triHeight/2.
-        poly.setAttributeNS(null, "points",
+      var arrowTriangleRatio = targetSize / (settings("directedGraphArrowRatio") || 4);
+      var arrowTriangleWidth =  arrowTriangleRatio *
+        (settings("directedGraphArrowWidth") || 3);
+      var arrowTriangleHeight = arrowTriangleRatio *
+        (settings("directedGraphArrowHeight") || 5);
+
+      var endX = targetX;
+      var endY = targetY;
+      // Whichever distance is smaller is the point at which the line exits
+      // the rectangle. This is therefore where we want to place the arrow.
+      if (Math.abs(distToEndWidth) > Math.abs(distToEndHeight)) {
+          // The exact x,y coordinate pair of where the line exits through
+          // the top/bottom of the rectangle.
+          endX += ((distToEndHeight > 0) ? 1 : -1) * distToEndHeight * cosAng;
+          endY += ((distToEndHeight > 0) ? 1 : -1) * targetSize;
+      } else {
+          // The exact x,y coordinate pair of where the line exits through
+          // the left/right of the rectangle.
+          endX += ((distToEndWidth > 0) ? 1 : -1) * targetSize;
+          endY += ((distToEndWidth > 0) ? 1 : -1) * distToEndWidth * sinAng;
+      }
+
+      var angle = Math.asin(sinAng) * 180 / Math.PI;
+      // Account for which side the arrow is coming from.
+      if (distToEndWidth > 0) {
+          angle = 180 - angle;
+      }
+      // Draw a triangle on the screen. The arrow head is pointing to endX,endY.
+      // The other two points are placed to the left, at a
+      // height of + triHeight/2 and - triHeight/2.
+      poly.setAttributeNS(null, "points",
         (endX - arrowTriangleWidth) + "," + (endY + arrowTriangleHeight / 2) + " " +
         (endX - arrowTriangleWidth) + "," + (endY - arrowTriangleHeight / 2) + " " +
         endX + "," + endY);
-        // The triangle is then rotated around the arrow head to point in the
-        // same direction as the line.
-        poly.setAttributeNS(null, "transform", "rotate(" + (-angle) + ", " +
+      // The triangle is then rotated around the arrow head to point in the
+      // same direction as the line.
+      poly.setAttributeNS(null, "transform", "rotate(" + (-angle) + ", " +
         endX + ", " + endY + ")");
 
-        return this;
+      return this;
     }
   };
 })();
